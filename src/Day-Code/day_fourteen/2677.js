52696b396b750ba73e61ebb4457556206b80078e
jahid-111 <jahidjob5@outlook.com>
2025-05-16 01:17:25 +0300

    try-with-forOf-loop_and_code-useCases

--- src/Day-Code/day_fourteen/2677.js
+++ src/Day-Code/day_fourteen/2677.js
@@ -12,9 +12,58 @@ function chunkArray(arr, size) {
   return chunked;
 }
 
-const jsonInput = "[1, 4, 60, 4, 5]";
-const arr = JSON.parse(jsonInput);
-const size = 1;
+// For of loop
 
-console.log(chunkArray(arr, size));
-// Output: [ [1, 2, 3], [4, 5, 6], [7] ]
+function chunkArrays(arr, size) {
+  let chunked = [];
+  let currentChunk = []; // [1,4,60,4,5]
+
+  for (const element of arr) {
+    currentChunk.push(element);
+
+    if (currentChunk.length === size) {
+      chunked.push(currentChunk);
+      currentChunk = [];
+    }
+  }
+  if (currentChunk.length > 0) {
+    chunked.push(currentChunk);
+    // console.log(chunked); //[52]
+  }
+
+  return chunked;
+}
+const jsonInput = "[1, 4, 60, 4, 5,6,3,2,52]";
+const arr = JSON.parse(jsonInput);
+const size = 2;
+
+console.log(chunkArrays(arr, size));
+// Output: [ [ 1, 4 ], [ 60, 4 ], [ 5, 6 ], [ 3, 2 ], [ 52 ] ]
+
+// ======================
+//----------------------------------------- Project Use Cases
+// ======================
+
+// --------------------- 1. Pagination (Frontend/React)
+
+// const paginatedItems = chunkArrays(data, itemsPerPage);
+
+// const items = [
+//   /* large dataset */
+// ];
+// const pageSize = 10;
+// const pages = chunkArrays(items, pageSize);
+
+// // current page
+// const currentPage = 1;
+// const visibleItems = pages[currentPage - 1];
+
+// --------------------- 2. Grid or Card Layouts
+/* const rows = chunkArrays(cards, 3);
+
+rows.map((row, index) => (
+  <div className="row" key={index}>
+    {row.map(card => <CardComponent key={card.id} {...card} />)}
+  </div>
+));
+ */
